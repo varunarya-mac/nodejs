6232564c3664af6de6bc96a7c593596891c7c491
Varun Arya <varun.arya@192.168.1.191>
2022-09-19 17:02:19 +0100

    added API for product items

--- dummyServer/src/index.ts
+++ dummyServer/src/index.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = 8080;
+var fs = require('fs');
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+
+app.get('/getItems', (req, res) => {
+  
+  const itemsFilePath = path.join(__dirname, '/assets/items.json');
+  fs.readFile(itemsFilePath, (fileErr: Error, result: any) => {
+    if(fileErr) throw fileErr;
+
+    var jsonData = JSON.parse(result.toString());
+    console.log(JSON.parse(result.toString()));
+    res.json(jsonData);
+});
 });
 
 app.listen(port, () => {
